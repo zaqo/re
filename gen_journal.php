f4ce1d5eb7ac0205ac2a61e25e556f2d0a00f400
zaqo <zaqo@narod.ru>
2017-11-02 17:04:14 +0300

    Gen invoices 11 02

--- gen_journal.php
+++ gen_journal.php
@@ -12,7 +12,7 @@ include_once("header_tpl_doc.php");
 
 			<!-- Header -->
 			<div class="w3-container" style="margin-top:0px" id="showcase">
-				<h1 class="w3-jumbo w3-text-grey"><b>#МЕДОСМОСТР</b></h1>
+				<h1 class="w3-jumbo w3-text-grey"><b>#МЕДОСМОТР</b></h1>
 				<h1 class="w3-xxxlarge w3-text-red"><b>журнал выдачи счетов про-форма.</b></h1>
 				<hr style="width:50px;border:5px solid red" class="w3-round">
 				</div>';
